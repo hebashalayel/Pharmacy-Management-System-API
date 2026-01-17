ddba3128ba45d51560be90141bad0e7b8d26bd17
Heba Shalayel <hebakhshalayel@gmail.com>
2026-01-17 13:07:33 +0200

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,9 @@ module.exports = {
     pharmacyController: require('./pharmacy'),
     authController: require('./auth'),
     primaryMedicineController: require('./primaryMedicine'),
-    alternativeMedicineController:require('./alternativeMedicine') 
-    
+    alternativeMedicineController:require('./alternativeMedicine')   
 }
+
 
 
 
